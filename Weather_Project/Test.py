f6d69b35d892102e2aa7f979132e007d0f46edaa
SVivekkrishnamurthy <1983.vivek@gmail.com>
2024-01-15 13:39:27 +0530

    Transformastion

--- Weather_Project/Test.py
+++ Weather_Project/Test.py
@@ -1,3 +1,13 @@
 import streamlit as st 
-st.write("Python")
-st.markdown("<h1> Rajalakshmi Edu </h1>", unsafe_allow_html=True)
+# Title of the app
+st.title("My Streamlit App")
+
+# Adding a text input widget
+user_input = st.text_input("Enter your name", "John Doe")
+
+# Displaying a message
+st.write(f"Hello, {user_input}!")
+
+# Adding a button
+if st.button("Click me"):
+    st.write("Button clicked!")
